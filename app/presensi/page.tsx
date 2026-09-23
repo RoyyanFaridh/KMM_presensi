f6c0630c533633e6a14cdfb525fd64128adfeda5
RoyyanFaridh <royyanfaridhnashruddin@gmail.com>
2026-09-23 17:15:35 +0700

    delete presensi page public

--- app/presensi/page.tsx
+++
@@ -1,9 +0,0 @@
-import CheckinFlow from '../../src/components/presensi/CheckinFlow'
-
-export default function Page() {
-  return (
-    <main className="min-h-screen bg-gray-50">
-      <CheckinFlow />
-    </main>
-  )
-}
